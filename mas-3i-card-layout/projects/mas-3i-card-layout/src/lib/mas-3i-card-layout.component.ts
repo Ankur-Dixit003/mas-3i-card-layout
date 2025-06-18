9ab24fd45d24a1b2b695e358710febd54e17b780
Ankur Dixit <95397540+Ankur-Dixit003@users.noreply.github.com>
2025-06-19 08:37:38 +0900

    Mas3i-card-layout package (#1)
    * Push codes of mas3i-card-layout
    
    * Update package.json

---
+++ mas-3i-card-layout/projects/mas-3i-card-layout/src/lib/mas-3i-card-layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mas-3i-card-layout',
+    standalone: true,
+    imports: [],
+    template: `
+    <p>
+      mas-3i-card-layout works!
+    </p>
+  `,
+    styles: ``
+})
+export class CardLayoutComponent {
+
+}
